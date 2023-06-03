2e41292d044e8792dac069cccb19240d9c9f8958
bassel97 <bassel.mks97@gmail.com>
2023-06-03 20:08:40 +0300

    Initial engine commit

---
+++ Source/NDanger/GameMode/NDGameModeBase.cpp
@@ -0,0 +1,4 @@
+// Created by Bassel Mostafa https://bassel97.github.io/ All Rights Reserved.
+
+#include "NDGameModeBase.h"
+
---
+++ Source/NDanger/GameMode/NDGameModeBase.h
@@ -0,0 +1,17 @@
+// Created by Bassel Mostafa https://bassel97.github.io/ All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NDGameModeBase.generated.h"
+
+/**
+ * Base class of all game modes in the project.
+ */
+UCLASS()
+class NDANGER_API ANDGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
